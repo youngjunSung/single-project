511c1335ef9772db20ab0fe740362b0bf87cae0d
yj <seong0814@naver.com>
2018-10-16 00:51:36 +0900

    홈작업

--- assets/js/pages/index.js
+++ assets/js/pages/index.js
@@ -1,32 +1,5 @@
-$(function () {
-    new Swiper('.swiper-main', {
-        slidesPerView: 7.8,
-        spaceBetween: 30,
-        centeredSlides: true,
-        grabCursor: true,
-        loop: true,
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        }
-    });
-
-    new Swiper('.swiper-sub', {
-        slidesPerView: 3,
-        spaceBetween: 10,
-        grabCursor: true,
-        loop: true,
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        }
-    });
+$(document).ready(function () {
+   $(window).resize(function () {
+       $('.menu-toggle, .toc-list').removeClass('active');
+   });
 });
