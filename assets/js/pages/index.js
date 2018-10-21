a23d797fb47eb2d136ff04a7c173e30f8e7e58f6
yj <seong0814@naver.com>
2018-10-22 00:41:27 +0900

    홈작업

--- assets/js/pages/index.js
+++ assets/js/pages/index.js
@@ -1,5 +1,34 @@
 $(document).ready(function () {
-   $(window).resize(function () {
-       $('.menu-toggle, .toc-list').removeClass('active');
-   });
+    $(window).resize(function () {
+        var winWidth = $(document).width(),
+            winWidth2 = window.innerWidth,
+            winWidth3 = window.outerWidth,
+            winHeight = $(window).height();
+        // Header control
+        if (winWidth2 >= 990) {
+            $('.menu-toggle, .toc-list, .header-bg-mo').removeClass('active');
+        }
+    });
+
+    // Header control
+    $('.header-bg-mo').click(function () {
+        $('.menu-toggle, .toc-list, .header-bg-mo').removeClass('active');
+    });
+    $('.toggle-btn-pc').click(function () {
+       $(this).toggleClass('active');
+       $('.toc-list').toggleClass('pc-hidden');
+       $('.content-wrap').toggleClass('pc-hidden');
+    });
+
+    // 첫 로드 시 content 제어
+    $('.content-inner').not(':first').hide();
+
+    // content 이동
+    $('.toc-menu p').click(function () {
+       var $date_name = $(this).text();
+       $('.content-inner').hide();
+       $('.content-inner[data-name="'+$date_name+'"]').fadeIn();
+       $('.menu-toggle, .toc-list, .header-bg-mo').removeClass('active');
+       $(window).scrollTop(0);
+    });
 });
