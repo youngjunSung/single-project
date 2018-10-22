9c379b7a8c5880385ec1a478329fbb019f42b35b
yj <seong0814@naver.com>
2018-10-23 00:03:15 +0900

    홈수정

--- assets/js/pages/index.js
+++ assets/js/pages/index.js
@@ -6,30 +6,28 @@ $(document).ready(function () {
             winHeight = $(window).height();
         // Header control
         if (winWidth2 >= 990) {
-            $('.menu-toggle, .toc-list, .header-bg-mo').removeClass('active');
+            $('.menu-toggle, .toc-list, .bg-mo').removeClass('active');
         }
     });
 
     // Header control
-    $('.header-bg-mo').click(function () {
-        $('.menu-toggle, .toc-list, .header-bg-mo').removeClass('active');
+    $('.bg-mo').click(function () {
+        $('.menu-toggle, .toc-list, .bg-mo').removeClass('active');
     });
     $('.toggle-btn-pc').click(function () {
        $(this).toggleClass('active');
-       $('.toc-list').toggleClass('pc-hidden');
-       $('.content-wrap').toggleClass('pc-hidden');
+       $('.toc-list, .content-wrap').toggleClass('pc-hidden');
     });
 
     // 첫 로드 시 content 제어
-    // $('.content-inner').not('[data-name="Component Recycle"]').hide();
-    $('.content-inner').not(':eq(1)').hide();
+    $('.content-inner').not('[data-name="Component Recycle"]').hide();
 
     // content 이동
     $('.toc-menu p').click(function () {
        var $date_name = $(this).text();
        $('.content-inner').hide();
        $('.content-inner[data-name="'+$date_name+'"]').fadeIn();
-       $('.menu-toggle, .toc-list, .header-bg-mo').removeClass('active');
+       $('.menu-toggle, .toc-list, .bg-mo').removeClass('active');
        $(window).scrollTop(0);
     });
 });
